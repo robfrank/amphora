f75167debb9a8591aaa3992a55d1af41b9639872
Roberto FRANK Franchini <ro.franchini@gmail.com>
2012-05-23 10:53:02 +0200

    renamed to Amphora

---
+++ src/main/java/it/jugtorino/chalk/Amphora.java
@@ -0,0 +1,250 @@
+package it.jugtorino.chalk;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import lombok.ToString;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static it.jugtorino.chalk.Joiners.onSpaceJoiner;
+import static it.jugtorino.chalk.Objects.isNotNull;
+import static it.jugtorino.chalk.Objects.isNotSameObject;
+import static it.jugtorino.chalk.Objects.isNull;
+import static it.jugtorino.chalk.Objects.isSameObject;
+import static it.jugtorino.chalk.Objects.not;
+import static com.google.common.base.Functions.toStringFunction;
+import static com.google.common.base.Predicates.contains;
+
+import static com.google.common.collect.Collections2.filter;
+import static com.google.common.collect.Collections2.transform;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+
+@ToString
+public class Amphora {
+
+	public static final Amphora ERROR = new Amphora();
+
+	private final ListMultimap<String, Object> data;
+
+	public Amphora() {
+		super();
+		this.data = ArrayListMultimap.create();
+	}
+
+	public Collection<Entry<String, Object>> entries() {
+		return data.entries();
+	}
+
+	public String valueOf(String fieldName) {
+		Object rawValue = rawValueOf(fieldName);
+
+		if (Objects.isNull(rawValue)) return EMPTY;
+		return rawValue.toString();
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends Object> T rawValueOf(String fieldName) {
+		if (data.get(fieldName).isEmpty()) return null;
+		return (T) data.get(fieldName).get(0);
+	}
+
+	public Collection<String> valuesOf(String fieldName) {
+		return transform(data.get(fieldName), toStringFunction());
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends Object> Collection<T> rawValuesOf(String fieldName) {
+		return (Collection<T>) data.get(fieldName);
+	}
+
+	public Set<String> fields() {
+		return newHashSet(data.keySet());
+	}
+
+	public Set<String> fields(Pattern pattern) {
+		return newHashSet(filter(data.keySet(), contains(pattern)));
+	}
+
+	public Amphora addAll(String fieldName, Iterable<? extends Object> values) {
+		for (Object value : values) {
+			add(fieldName, value);
+		}
+		return this;
+	}
+
+	public Amphora load(Map<String, ? extends Object> data) {
+		for (Entry<String, ?> entry : data.entrySet()) {
+			Object value = isNull(entry.getValue()) ? EMPTY : entry.getValue();
+			add(entry.getKey(), value);
+		}
+		return this;
+	}
+
+	public Amphora add(String fieldName, Object fieldValue) {
+		if (isNotNull(fieldValue)) data.put(fieldName, fieldValue);
+
+		return this;
+	}
+
+	public Amphora replace(String field, Object newValue) {
+		remove(field);
+		add(field, newValue);
+		return this;
+	}
+
+	public Amphora remove(String fieldName, Object fieldValue) {
+		data.remove(fieldName, fieldValue);
+		return this;
+	}
+
+	public Amphora remove(String fieldName) {
+		data.removeAll(fieldName);
+		return this;
+	}
+
+	public boolean isError() {
+		return isSameObject(this, ERROR);
+	}
+
+	public boolean isNotError() {
+		return isNotSameObject(this, ERROR);
+	}
+
+	public boolean hasField(String fieldName) {
+		return data.containsKey(fieldName);
+	}
+
+	public boolean hasNotField(String fieldName) {
+		return not(data.containsKey(fieldName));
+	}
+
+	public boolean hasValue(Object value) {
+		return data.containsValue(value);
+	}
+
+	public boolean hasValue(String fieldName, Object value) {
+		return data.containsEntry(fieldName, value);
+	}
+
+	public boolean hasNotValue(Object value) {
+		return not(data.containsValue(value));
+	}
+
+	public boolean hasNotValue(String fieldName, Object value) {
+		return not(data.containsEntry(fieldName, value));
+	}
+
+	public Amphora copy(String fieldName, String newFieldName) {
+		Collection<Object> values = newArrayList(data.get(fieldName));
+		addAll(newFieldName, values);
+		return this;
+	}
+
+	public Amphora rename(String oldFieldName, String newFieldName) {
+		if (oldFieldName.equals(newFieldName)) return this;
+
+		copy(oldFieldName, newFieldName);
+		remove(oldFieldName);
+		return this;
+	}
+
+	public Amphora rename(Map<String, String> old2new) {
+		for (Entry<String, String> mapping : old2new.entrySet()) {
+			rename(mapping.getKey(), mapping.getValue());
+		}
+		return this;
+	}
+
+	public Amphora mergeValuesOf(String fieldName) {
+		mergeValuesOf(fieldName, onSpaceJoiner);
+		return this;
+	}
+
+	public Amphora mergeValuesOf(String fieldName, Joiner joiner) {
+		String merged = joiner.join(valuesOf(fieldName));
+
+		remove(fieldName);
+
+		add(fieldName, merged);
+		return this;
+
+	}
+
+	public Amphora split(String fieldName) {
+		split(fieldName, Splitter.on(' '));
+
+		return this;
+
+	}
+
+	public Amphora split(String fieldName, Splitter splitter) {
+		Iterable<String> splitted = splitter.split(valueOf(fieldName));
+
+		remove(fieldName);
+
+		addAll(fieldName, splitted);
+		return this;
+
+	}
+
+	public Amphora merge(Amphora toMerge) {
+		for (String field : toMerge.fields()) {
+			addAll(field, toMerge.rawValuesOf(field));
+		}
+		return this;
+	}
+
+	public Amphora clear() {
+		data.clear();
+		return this;
+	}
+
+	public Amphora modify(String fieldName, Function fieldModifier) {
+		replace(fieldName, fieldModifier.apply(rawValueOf(fieldName)));
+		return this;
+	}
+
+	public Amphora apply(String from, Function transformer, String to) {
+		add(to, transformer.apply(rawValueOf(from)));
+		return this;
+	}
+
+	public Map<String, Collection<Object>> asRawMap() {
+		return data.asMap();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+
+		Amphora other = (Amphora) obj;
+
+		if (data == null) {
+			if (other.data != null) return false;
+		} else if (!data.equals(other.data)) return false;
+
+		return true;
+	}
+
+}
---
+++ src/test/java/it/jugtorino/chalk/AmphoraTest.java
@@ -0,0 +1,258 @@
+package it.jugtorino.chalk;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMap;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import static it.jugtorino.chalk.Joiners.onMinusAndSpacesJoiner;
+import static it.jugtorino.chalk.Joiners.onSpaceJoiner;
+import static com.google.common.base.Joiner.on;
+
+public class AmphoraTest {
+
+	@Test
+	public void shouldRenameMultiValuesFields() throws Exception {
+
+		Amphora amphora = new Amphora()
+				.add("name", "first")
+				.add("name", "second")
+				.add("name", "third");
+
+		amphora.rename("name", "newName");
+		assertTrue(amphora.hasField("newName"));
+		assertTrue(amphora.hasNotField("name"));
+
+		assertThat(onSpaceJoiner.join(amphora.valuesOf("newName")), equalTo("first second third"));
+	}
+
+	@Test
+	public void shouldRenameMultipleFields() throws Exception {
+		Amphora amphora = new Amphora()
+				.add("name", "first")
+				.add("name", "second")
+				.add("name", "third")
+				.add("name2", "four");
+
+		amphora.rename(ImmutableMap.of("name", "newName", "name2", "newName2"));
+
+		assertTrue(amphora.hasField("newName"));
+		assertTrue(amphora.hasNotField("name"));
+
+		assertThat(on(" ").join(amphora.valuesOf("newName")), equalTo("first second third"));
+
+		assertTrue(amphora.hasField("newName2"));
+		assertTrue(amphora.hasNotField("name2"));
+
+		assertThat(on(" ").join(amphora.valuesOf("newName2")), equalTo("four"));
+
+	}
+
+	@Test
+	public void shouldMergeMultiValuesFields() throws Exception {
+		Amphora amphora = new Amphora()
+				.add("name", "first")
+				.add("name", "second")
+				.add("name", "third");
+
+		amphora.mergeValuesOf("name");
+
+		assertTrue(amphora.hasField("name"));
+
+		assertThat(amphora.valueOf("name"), equalTo("first second third"));
+	}
+
+	@Test
+	public void shouldMergeMultiValuesFieldsWithGivenJoiner() throws Exception {
+		Amphora amphora = new Amphora()
+				.add("name", "first")
+				.add("name", "second")
+				.add("name", "third");
+
+		amphora.mergeValuesOf("name", onMinusAndSpacesJoiner);
+
+		assertTrue(amphora.hasField("name"));
+
+		assertThat(amphora.valueOf("name"), equalTo("first - second - third"));
+	}
+
+	@Test
+	public void amphorasWithSameValuesAreEquals() {
+
+		Amphora first = new Amphora()
+				.add("name", "value")
+				.add("name", "value");
+
+		Amphora second = new Amphora()
+				.add("name", "value")
+				.add("name", "value");
+
+		assertThat(second, equalTo(first));
+
+		second.add("difference", "different");
+		assertThat(second, not(equalTo(first)));
+
+	}
+
+	@Test
+	public void amphoraWithRawValues() throws Exception {
+
+		Amphora amphora2 = new Amphora();
+
+		Amphora amphora = new Amphora()
+				.add("stringVal", "this is a string")
+				.add("anotheramphora", amphora2);
+
+		assertThat(amphora.valueOf("stringVal"), equalTo("this is a string"));
+
+		String rawValueOf = amphora.rawValueOf("stringVal");
+		assertThat(rawValueOf, equalTo("this is a string"));
+
+		assertThat(amphora.valueOf("anotheramphora"), equalTo(amphora2.toString()));
+		Amphora anotheramphora = amphora.rawValueOf("anotheramphora");
+		assertThat(anotheramphora, equalTo(amphora2));
+	}
+
+	@Test
+	public void shouldFilterFiledsKeyByRegExp() throws Exception {
+		Amphora amphora = new Amphora();
+
+		amphora.add("prefix_me", "value");
+		amphora.add("prefix_metoo", "value");
+		amphora.add("meno_suffix", "value");
+
+		Set<String> fields = amphora.fields(Pattern.compile("prefix_.*"));
+
+		assertEquals(2, fields.size());
+		assertTrue(fields.contains("prefix_me"));
+		assertTrue(fields.contains("prefix_metoo"));
+
+	}
+
+	@Test
+	public void shouldMergeamphoras() throws Exception {
+		Amphora amphora = new Amphora();
+		amphora.add("name", "value1");
+		amphora.add("name", "value2");
+
+		Amphora toMerge = new Amphora();
+		toMerge.add("name2", "value21");
+		toMerge.add("name2", "value22");
+
+		amphora.merge(toMerge);
+
+		assertTrue(amphora.hasField("name2"));
+		assertEquals(2, amphora.valuesOf("name2").size());
+
+	}
+
+	@Test
+	public void shouldReplaceFieldValue() throws Exception {
+		Amphora amphora = new Amphora()
+				.add("name", "value1")
+				.add("name", "value2");
+
+		amphora.replace("name", "newValue");
+
+		assertEquals("newValue", amphora.valueOf("name"));
+		assertEquals(1, amphora.valuesOf("name").size());
+
+		amphora = new Amphora()
+				.add("name", "value1");
+
+		amphora.replace("name", Boolean.valueOf(amphora.hasField("name")));
+
+		assertThat(amphora.valueOf("name"), equalTo("true"));
+		assertEquals(1, amphora.valuesOf("name").size());
+
+	}
+
+	@Test
+	public void shouldApplyFunctionToStringField() throws Exception {
+		Amphora amphora = new Amphora();
+
+		Function<String, String> fieldModifier = new Function<String, String>() {
+
+			@Override
+			public String apply(String input) {
+
+				return "MODIFIED";
+			}
+		};
+
+		amphora.add("name", "value1");
+
+		amphora.modify("name", fieldModifier);
+
+		assertThat(amphora.valueOf("name"), equalTo("MODIFIED"));
+
+	}
+
+	@Test
+	public void shouldApplyFunctionToObjectField() throws Exception {
+		Amphora amphora = new Amphora();
+
+		Function<Integer, Integer> fieldModifier = new Function<Integer, Integer>() {
+
+			@Override
+			public Integer apply(Integer input) {
+				return Integer.valueOf(2);
+			}
+		};
+
+		amphora.add("name", Integer.valueOf(1));
+
+		amphora.modify("name", fieldModifier);
+
+		assertThat(amphora.valueOf("name"), equalTo("2"));
+		assertThat((Integer) amphora.rawValueOf("name"), equalTo(2));
+		assertEquals("2", amphora.valueOf("name"));
+		assertEquals(2, amphora.rawValueOf("name"));
+
+	}
+
+	@Test
+	public void etlExample() throws Exception {
+		Function<Integer, Integer> incrementer = new Function<Integer, Integer>() {
+
+			@Override
+			public Integer apply(Integer input) {
+				return ++input;
+			}
+		};
+
+		Amphora amphora = new Amphora()
+				.add("incrementMe", Integer.valueOf(1))
+				.add("txt", "a very long text")
+				.add("joinMe", "multi1")
+				.add("joinMe", "multi2")
+				.add("joinMe", "multi3")
+				.add("joinMe", "multi4")
+				.add("joinMe", "multi5")
+				.add("splitMe", "one two three four")
+				.add("renameME", new Amphora().add("name", "inner"));
+
+		System.out.println(amphora);
+
+		amphora.modify("incrementMe", incrementer)
+				.mergeValuesOf("joinMe", Joiners.onSpaceJoiner)
+				.split("splitMe")
+				.rename("renameME", "renamed");
+
+		System.out.println(amphora);
+
+		Amphora littleAmphora = amphora.rawValueOf("renamed");
+		Integer incremented = amphora.rawValueOf("incrementMe");
+
+	}
+}
