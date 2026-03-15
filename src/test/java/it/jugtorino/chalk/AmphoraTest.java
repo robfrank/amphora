a27210268b336f2e5ebe4411a63343487b1b1575
robfrank <ro.franchini@gmail.com>
2026-03-15 11:23:34 +0100

    test: migrate to JUnit 5 and AssertJ, modernize test syntax

--- src/test/java/it/jugtorino/chalk/AmphoraTest.java
+++ src/test/java/it/jugtorino/chalk/AmphoraTest.java
@@ -1,222 +1,181 @@
 package it.jugtorino.chalk;
 
-import static com.google.common.base.Joiner.on;
 import static it.jugtorino.chalk.Joiners.onMinusAndSpacesJoiner;
 import static it.jugtorino.chalk.Joiners.onSpaceJoiner;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.base.Function;
-import com.google.common.collect.ImmutableMap;
-import java.util.Set;
+import java.util.Map;
 import java.util.regex.Pattern;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class AmphoraTest {
+class AmphoraTest {
 
   @Test
-  public void shoudBeAnErrorAmphora() {
-    assertTrue(Amphora.ERROR.isError());
-    assertFalse(Amphora.ERROR.isNotError());
+  void shouldBeAnErrorAmphora() {
+    assertThat(Amphora.ERROR.isError()).isTrue();
+    assertThat(Amphora.ERROR.isNotError()).isFalse();
   }
 
   @Test
-  public void shouldRenameMultiValuesFields() throws Exception {
-
-    Amphora amphora = new Amphora().add("name", "first").add("name", "second").add("name", "third");
+  void shouldRenameMultiValuesFields() {
+    var amphora = new Amphora().add("name", "first").add("name", "second").add("name", "third");
 
     amphora.rename("name", "newName");
-    assertTrue(amphora.hasField("newName"));
-    assertTrue(amphora.hasNotField("name"));
+    assertThat(amphora.hasField("newName")).isTrue();
+    assertThat(amphora.hasNotField("name")).isTrue();
 
-    assertThat(onSpaceJoiner.join(amphora.valuesOf("newName")), equalTo("first second third"));
+    assertThat(onSpaceJoiner.join(amphora.valuesOf("newName"))).isEqualTo("first second third");
   }
 
   @Test
-  public void shouldRenameMultipleFields() throws Exception {
-    Amphora amphora =
+  void shouldRenameMultipleFields() {
+    var amphora =
         new Amphora()
             .add("name", "first")
             .add("name", "second")
             .add("name", "third")
             .add("name2", "four");
 
-    amphora.rename(ImmutableMap.of("name", "newName", "name2", "newName2"));
+    amphora.rename(Map.of("name", "newName", "name2", "newName2"));
 
-    assertTrue(amphora.hasField("newName"));
-    assertTrue(amphora.hasNotField("name"));
+    assertThat(amphora.hasField("newName")).isTrue();
+    assertThat(amphora.hasNotField("name")).isTrue();
 
-    assertThat(on(" ").join(amphora.valuesOf("newName")), equalTo("first second third"));
+    assertThat(String.join(" ", amphora.valuesOf("newName"))).isEqualTo("first second third");
 
-    assertTrue(amphora.hasField("newName2"));
-    assertTrue(amphora.hasNotField("name2"));
+    assertThat(amphora.hasField("newName2")).isTrue();
+    assertThat(amphora.hasNotField("name2")).isTrue();
 
-    assertThat(on(" ").join(amphora.valuesOf("newName2")), equalTo("four"));
+    assertThat(String.join(" ", amphora.valuesOf("newName2"))).isEqualTo("four");
   }
 
   @Test
-  public void shouldMergeMultiValuesFields() throws Exception {
-    Amphora amphora = new Amphora().add("name", "first").add("name", "second").add("name", "third");
+  void shouldMergeMultiValuesFields() {
+    var amphora = new Amphora().add("name", "first").add("name", "second").add("name", "third");
 
     amphora.mergeValuesOf("name");
 
-    assertTrue(amphora.hasField("name"));
-
-    assertThat(amphora.valueOf("name"), equalTo("first second third"));
+    assertThat(amphora.hasField("name")).isTrue();
+    assertThat(amphora.valueOf("name")).isEqualTo("first second third");
   }
 
   @Test
-  public void shouldMergeMultiValuesFieldsWithGivenJoiner() throws Exception {
-    Amphora amphora = new Amphora().add("name", "first").add("name", "second").add("name", "third");
+  void shouldMergeMultiValuesFieldsWithGivenJoiner() {
+    var amphora = new Amphora().add("name", "first").add("name", "second").add("name", "third");
 
     amphora.mergeValuesOf("name", onMinusAndSpacesJoiner);
 
-    assertTrue(amphora.hasField("name"));
-
-    assertThat(amphora.valueOf("name"), equalTo("first - second - third"));
+    assertThat(amphora.hasField("name")).isTrue();
+    assertThat(amphora.valueOf("name")).isEqualTo("first - second - third");
   }
 
   @Test
-  public void amphorasWithSameValuesAreEquals() {
+  void amphorasWithSameValuesAreEquals() {
+    var first = new Amphora().add("name", "value").add("name", "value");
+    var second = new Amphora().add("name", "value").add("name", "value");
 
-    Amphora first = new Amphora().add("name", "value").add("name", "value");
-
-    Amphora second = new Amphora().add("name", "value").add("name", "value");
-
-    assertThat(second, equalTo(first));
+    assertThat(second).isEqualTo(first);
 
     second.add("difference", "different");
-    assertThat(second, not(equalTo(first)));
+    assertThat(second).isNotEqualTo(first);
   }
 
   @Test
-  public void amphoraWithRawValues() throws Exception {
+  void amphoraWithRawValues() {
+    var amphora2 = new Amphora();
+    var amphora = new Amphora().add("stringVal", "this is a string").add("anotheramphora", amphora2);
 
-    Amphora amphora2 = new Amphora();
-
-    Amphora amphora =
-        new Amphora().add("stringVal", "this is a string").add("anotheramphora", amphora2);
-
-    assertThat(amphora.valueOf("stringVal"), equalTo("this is a string"));
+    assertThat(amphora.valueOf("stringVal")).isEqualTo("this is a string");
 
     String rawValueOf = amphora.rawValueOf("stringVal");
-    assertThat(rawValueOf, equalTo("this is a string"));
+    assertThat(rawValueOf).isEqualTo("this is a string");
 
-    assertThat(amphora.valueOf("anotheramphora"), equalTo(amphora2.toString()));
+    assertThat(amphora.valueOf("anotheramphora")).isEqualTo(amphora2.toString());
     Amphora anotheramphora = amphora.rawValueOf("anotheramphora");
-    assertThat(anotheramphora, equalTo(amphora2));
+    assertThat(anotheramphora).isEqualTo(amphora2);
   }
 
   @Test
-  public void shouldFilterFiledsKeyByRegExp() throws Exception {
-    Amphora amphora = new Amphora();
+  void shouldFilterFieldsKeyByRegExp() {
+    var amphora = new Amphora();
 
     amphora.add("prefix_me", "value");
     amphora.add("prefix_metoo", "value");
     amphora.add("meno_suffix", "value");
 
-    Set<String> fields = amphora.fields(Pattern.compile("prefix_.*"));
+    var fields = amphora.fields(Pattern.compile("prefix_.*"));
 
-    assertEquals(2, fields.size());
-    assertTrue(fields.contains("prefix_me"));
-    assertTrue(fields.contains("prefix_metoo"));
+    assertThat(fields).hasSize(2).containsExactlyInAnyOrder("prefix_me", "prefix_metoo");
   }
 
   @Test
-  public void shouldMergeamphoras() throws Exception {
-    Amphora amphora = new Amphora();
+  void shouldMergeAmphoras() {
+    var amphora = new Amphora();
     amphora.add("name", "value1");
     amphora.add("name", "value2");
 
-    Amphora toMerge = new Amphora();
+    var toMerge = new Amphora();
     toMerge.add("name2", "value21");
     toMerge.add("name2", "value22");
 
     amphora.merge(toMerge);
 
-    assertTrue(amphora.hasField("name2"));
-    assertEquals(2, amphora.valuesOf("name2").size());
+    assertThat(amphora.hasField("name2")).isTrue();
+    assertThat(amphora.valuesOf("name2")).hasSize(2);
   }
 
   @Test
-  public void shouldReplaceFieldValue() throws Exception {
-    Amphora amphora = new Amphora().add("name", "value1").add("name", "value2");
+  void shouldReplaceFieldValue() {
+    var amphora = new Amphora().add("name", "value1").add("name", "value2");
 
     amphora.replace("name", "newValue");
 
-    assertEquals("newValue", amphora.valueOf("name"));
-    assertEquals(1, amphora.valuesOf("name").size());
+    assertThat(amphora.valueOf("name")).isEqualTo("newValue");
+    assertThat(amphora.valuesOf("name")).hasSize(1);
 
     amphora = new Amphora().add("name", "value1");
 
     amphora.replace("name", Boolean.valueOf(amphora.hasField("name")));
 
-    assertThat(amphora.valueOf("name"), equalTo("true"));
-    assertEquals(1, amphora.valuesOf("name").size());
+    assertThat(amphora.valueOf("name")).isEqualTo("true");
+    assertThat(amphora.valuesOf("name")).hasSize(1);
   }
 
   @Test
-  public void shouldApplyFunctionToStringField() throws Exception {
-    Amphora amphora = new Amphora();
+  void shouldApplyFunctionToStringField() {
+    var amphora = new Amphora();
 
-    Function<String, String> fieldModifier =
-        new Function<String, String>() {
-
-          @Override
-          public String apply(String input) {
-
-            return "MODIFIED";
-          }
-        };
+    Function<String, String> fieldModifier = input -> "MODIFIED";
 
     amphora.add("name", "value1");
 
     amphora.modify("name", fieldModifier);
 
-    assertThat(amphora.valueOf("name"), equalTo("MODIFIED"));
+    assertThat(amphora.valueOf("name")).isEqualTo("MODIFIED");
   }
 
   @Test
-  public void shouldApplyFunctionToObjectField() throws Exception {
-    Amphora amphora = new Amphora();
+  void shouldApplyFunctionToObjectField() {
+    var amphora = new Amphora();
 
-    Function<Integer, Integer> fieldModifier =
-        new Function<Integer, Integer>() {
+    Function<Integer, Integer> fieldModifier = input -> 2;
 
-          @Override
-          public Integer apply(Integer input) {
-            return Integer.valueOf(2);
-          }
-        };
-
-    amphora.add("name", Integer.valueOf(1));
+    amphora.add("name", 1);
 
     amphora.modify("name", fieldModifier);
 
-    assertThat(amphora.valueOf("name"), equalTo("2"));
-    assertThat((Integer) amphora.rawValueOf("name"), equalTo(2));
-    assertEquals("2", amphora.valueOf("name"));
-    assertEquals(2, (int) amphora.rawValueOf("name"));
+    assertThat(amphora.valueOf("name")).isEqualTo("2");
+    assertThat((Integer) amphora.rawValueOf("name")).isEqualTo(2);
   }
 
   @Test
-  public void etlExample() throws Exception {
-    Function<Integer, Integer> incrementer =
-        new Function<Integer, Integer>() {
+  void etlExample() {
+    Function<Integer, Integer> incrementer = input -> ++input;
 
-          @Override
-          public Integer apply(Integer input) {
-            return ++input;
-          }
-        };
-
-    Amphora amphora =
+    var amphora =
         new Amphora()
-            .add("incrementMe", Integer.valueOf(1))
+            .add("incrementMe", 1)
             .add("txt", "a very long text")
             .add("joinMe", "multi1")
             .add("joinMe", "multi2")
@@ -228,14 +187,14 @@ public class AmphoraTest {
 
     amphora
         .modify("incrementMe", incrementer)
-        .mergeValuesOf("joinMe", Joiners.onSpaceJoiner)
+        .mergeValuesOf("joinMe", onSpaceJoiner)
         .split("splitMe")
         .rename("renameME", "renamed");
 
     Amphora littleAmphora = amphora.rawValueOf("renamed");
-    assertThat(littleAmphora.valueOf("name"), equalTo("inner"));
+    assertThat(littleAmphora.valueOf("name")).isEqualTo("inner");
 
     Integer incremented = amphora.rawValueOf("incrementMe");
-    assertThat(incremented, equalTo(2));
+    assertThat(incremented).isEqualTo(2);
   }
 }
